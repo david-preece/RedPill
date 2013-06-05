8e86f2235fa995d404af21b255ecab30aaab5a4e
meza <meza@meza.hu>
2013-06-05 14:23:30 +0100

    should return the score test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,21 +1,28 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
-	@Test
-	public void classUnderTestShouldExist() {
-		Game game = new Game();
+	private Game game;
+
+	@Before
+	public void setUp() {
+		game = new Game();
 	}
 
 	@Test
 	public void shouldReturnTheScore() {
-		Game game = new Game();
 		int expectedScore = 0;
 		int actualScore = game.score();
 
 		Assert.assertEquals("Score reported from the game is not what it should be", expectedScore,
 							actualScore);
+	}
+
+	@Test
+	public void shouldBeAbleToRoll() {
+		game.roll();
 	}
 
 }
