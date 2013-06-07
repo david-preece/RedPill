be35bc8f774a6020f46e34b052b77568b3aad60e
meza <meza@meza.hu>
2013-06-07 14:32:12 +0100

    Roman numerals kata. [6/8]

---
+++ src/test/java/RomanNumeralsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	@Test
+	public void classUnderTestShouldExist() {
+		RomanNumeralConverter converter = new RomanNumeralConverter();
+	}
+
+}
