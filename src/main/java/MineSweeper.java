81e8cbeba553d11ef346628d0a80b43aaf61d841
David Preece <david@dnaware.net>
2013-06-07 11:27:14 +0100

    GG

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -1,17 +1,11 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Dave&Zita
- * Date: 05/06/13
- * Time: 15:34
- * To change this template use File | Settings | File Templates.
- */
 public class MineSweeper {
     public String reveal(String input) {
 
         if (input.equals("."))
-            return "0"; // Player Missed!
-        else if (input.equals("*"))
-            return "*"; // Player Hit!
-        return null;  // Something went wrong, return null;
+            return "0";
+        else
+            input = input.replace ('.', '1');
+
+        return input;
     }
 }
