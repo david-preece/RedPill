959916ad8fc2930389bb0d393ab3f8e6671fc914
David Preece <david@dnaware.net>
2013-06-07 14:37:25 +0100

    Test passed

---
+++ src/main/java/RomanNumeralConverter.java
@@ -0,0 +1,8 @@
+/**
+ * Project: red-pill
+ * User: Dave
+ * Date: 07/06/13
+ * Time: 14:36
+ */
+public class RomanNumeralConverter {
+}
--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Dave
- * Date: 07/06/13
- * Time: 11:47
- * To change this template use File | Settings | File Templates.
- */
+
 public class StringCalculator {
     public int add(String inputString) {
         if (inputString.equals(""))
