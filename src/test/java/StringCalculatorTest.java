96522222b11406e94365e90287bcdac5abcf18ee
meza <meza@meza.hu>
2013-06-07 14:16:06 +0100

    string calculator should return 0 on empty string input. [5/8]

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,9 +1,26 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	private StringCalculator calculator;
+
+	@Before
+	public void setUp() throws Exception {
+		calculator = new StringCalculator();
+	}
+
 	@Test
-	public void classUnderTestShouldExist() {
+	public void emptyInputStringShouldResultInZero() {
 		StringCalculator calculator = new StringCalculator();
+
+		String inputString = "";
+		int expectedSum = 0;
+
+		int actualSum = calculator.add(inputString);
+
+		Assert.assertEquals("The calculator did not sum the numbers correctly with an empty input",
+							expectedSum, actualSum);
 	}
 }
