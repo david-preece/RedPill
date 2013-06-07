d364fe44276e97d5842a6e7a9a0427c5995a085a
David Preece <david@dnaware.net>
2013-06-07 11:48:24 +0100

    Added StringCalculator Class

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dave
+ * Date: 07/06/13
+ * Time: 11:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator {
+}
