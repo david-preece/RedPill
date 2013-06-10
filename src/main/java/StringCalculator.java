b0de744e7fb248d0b6f5611f1605aed87ad42b93
David Preece <david@dnaware.net>
2013-06-10 18:28:00 +0100

    I hate strings lol

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,7 +3,18 @@ public class StringCalculator {
     public int add(String inputString) {
         if (inputString.equals(""))
             return 0;
-        else
-            return 1;
+
+        // break up the string into an array
+        String[] brokeUpInput = inputString.split(",");
+
+        // Now add the numbers together
+        Integer result = 0;
+        for (int i = 0; i < brokeUpInput.length; i++)
+        {
+            result += Integer.parseInt(brokeUpInput[i]);
+        }
+
+        // Return result
+        return result;
     }
 }
