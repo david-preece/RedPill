9103e435026800757c021a2dd8fd077dbcd4ef12
meza <meza@meza.hu>
2013-06-10 18:43:17 +0100

    string calculator any number of test should be summed 3 [4/7]

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,9 +2,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class StringCalculatorTest {
 
 	private StringCalculator calculator;
+	private Random random = new Random();
 
 	@Before
 	public void setUp() throws Exception {
@@ -44,5 +47,35 @@ public class StringCalculatorTest {
 
 		Assert.assertEquals("The calculator did not sum the numbers correctly with 2 arguments", expectedSum,
 							actualSum);
+	}
+
+	@Test
+	public void unknownNumberOfArgumentsShouldBeAddedTogether() {
+		StringCalculator calculator = new StringCalculator();
+
+		StringBuilder sb = new StringBuilder();
+
+		int numberOfArguments = randomInt();
+		int expectedSum = 0;
+
+		for (int i = 1; i <= numberOfArguments; i++) {
+			final int input = randomInt();
+			sb.append(input);
+			if (i < numberOfArguments) {
+				sb.append(",");
+			}
+			expectedSum += input;
+		}
+
+		String inputString = sb.toString();
+
+		int actualSum = calculator.add(inputString);
+
+		Assert.assertEquals("The calculator did not sum the numbers correctly with " + numberOfArguments +
+							" number of arguments", expectedSum, actualSum);
+	}
+
+	private int randomInt() {
+		return random.nextInt(100);
 	}
 }
